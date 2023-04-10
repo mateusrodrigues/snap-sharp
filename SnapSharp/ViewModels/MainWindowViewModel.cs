23667ba7b38f706fbdf9271e81d99a882218f612
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2023-04-10 15:46:35 -0300

    avalonia project created

---
+++ SnapSharp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Reactive;
+using System.Reflection;
+using ReactiveUI;
+
+namespace SnapSharp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private string _greeting;
+    public string Greeting
+    {
+        get => _greeting;
+        set => this.RaiseAndSetIfChanged(ref _greeting, value);
+    }
+    
+    public ReactiveCommand<Unit, Unit> SayHelloCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        _greeting = "This is a snapped project!";
+        
+        SayHelloCommand = ReactiveCommand.Create(SayHello);
+    }
+
+    void SayHello()
+    {
+        var dotnetVersion = System.Reflection.Assembly
+            .GetExecutingAssembly()
+            .GetReferencedAssemblies()
+            .FirstOrDefault(a => a.Name == "System.Runtime")?
+            .Version?
+            .Major;
+        
+        Greeting = $"Hello, world from .NET {dotnetVersion}!";
+    }
+}
---
+++ SnapSharp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace SnapSharp.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
